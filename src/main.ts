b98e280e92707247703163c342eacdc6854e1902
Supertiger <saawalkhn@googlemail.com>
2025-08-01 18:25:28 +0100

    Refactor pathname handling for improved readability and consistency

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
-if (location.pathname === import.meta.env.BASE_URL + "/waybar") {
+const pathname = location.pathname.endsWith("/")
+  ? location.pathname.slice(0, -1)
+  : location.pathname;
+
+if (pathname === import.meta.env.BASE_URL + "/waybar") {
   import("./routes/waybar/waybar").then(({ createWaybarPage }) =>
     createWaybarPage()
   );
 }
 
-if (location.pathname === import.meta.env.BASE_URL) {
+if (pathname === import.meta.env.BASE_URL) {
   import("./routes/root/root").then(({ createRootPage }) => createRootPage());
 }
