2c0bf4953bf1a937d92a1f549f9f6d9893d36137
SupertigerDev <SupertigerDev@users.noreply.github.com>
2025-08-01 17:21:11 +0000

    deploy: 224e6ed9fdde9ca71adac5f54972817260b424d7

--- assets/root-QOTJvkXH.js
+++ assets/root-QOTJvkXH.js
@@ -1,3 +1,3 @@
 const e=document.getElementById("app"),a=async()=>{e.innerHTML=`
-    <iframe id="waybar-iframe" src="/waybar"></iframe>
+    <iframe id="waybar-iframe" src="/waybar-online/waybar"></iframe>
   `};export{a as createRootPage};
